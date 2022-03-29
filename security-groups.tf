5c40d936cbd849baedd2780b4480313f86104356
Mukesh Sharma <mukeshsharma24@gmail.com>
2022-03-29 15:30:16 +1300

    Added alb sg

--- security-groups.tf
+++ security-groups.tf
@@ -9,3 +9,19 @@ module "mysql_security_group" {
   auto_ingress_with_self = []
   ingress_cidr_blocks    = [module.vpc.vpc_cidr_block]
 }
+
+
+module "alb_security_group" {
+
+  source  = "terraform-aws-modules/security-group/aws//modules/https-443"
+  version = "4.3.0"
+
+  name        = "${var.environment}-alb-sg"
+  description = "Security group for ALB public access using port 443"
+  vpc_id      = module.vpc.vpc_id
+
+  auto_ingress_with_self = []
+
+  ingress_with_cidr_blocks = ["0.0.0.0/0"]
+
+}
